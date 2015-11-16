e2aa4aaf14de025901d790f3a251ac4688ed0ba6
Jacob Czech <czech.jacob@gmail.com>
2015-11-16 12:46:45 -0500

    Rename variables, rename bundle, use 7zip
    Since we have to use 7zip to create the final zip, we might as well use it
    to extract all the zip files instead of the custom function that was
    previously there.

--- windows/make_cellblender.ps1
+++ windows/make_cellblender.ps1
@@ -1,16 +1,7 @@
 ﻿# Change the project dir as needed
 $project_dir = "$home\bundle_cellblender\windows"
-$blender_download = "http://ftp.halifax.rwth-aachen.de/blender/release/Blender2.76/blender-2.76b-windows64.zip"
-$blender_destination = "$projectdir\blender.zip"
-
-# TODO: Just use 7zip to unzip, since we already need it to zip
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-function Unzip
-{
-    param([string]$zipfile, [string]$outpath)
-
-    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
-}
+$blender_download_url = "http://ftp.halifax.rwth-aachen.de/blender/release/Blender2.76/blender-2.76b-windows64.zip"
+$blender_zip = "$projectdir\blender.zip"
 
 cd $project_dir
 
@@ -18,20 +9,21 @@ cd $project_dir
 C:\Users\vagrant\.babun\cygwin\bin\bash.exe -login $project_dir\make_mcell.sh
 
 # Get Blender
-Invoke-WebRequest $blender_download -OutFile $blender_destination
-Unzip $blender_destination $project_dir
+Invoke-WebRequest $blender_download_url -OutFile $blender_zip
+& 'C:\Program Files\7-Zip\7z.exe' x $blender_zip -o"$project_dir"
 $blender_dir = "$project_dir\blender-2.76b-windows64"
 $addon_dir = "$blender_dir\2.76\scripts\addons"
 cd $addon_dir
 
 # Get CellBlender
-$cellblender_download = "https://github.com/mcellteam/cellblender/archive/development.zip"
-$cellblender_destination = "$addon_dir\cellblender.zip"
-Invoke-WebRequest $cellblender_download -Outfile $cellblender_destination
-Unzip $cellblender_destination $addon_dir
+$cellblender_download_url = "https://github.com/mcellteam/cellblender/archive/development.zip"
+$cellblender_zip = "$addon_dir\cellblender.zip"
+Invoke-WebRequest $cellblender_download_url -Outfile $cellblender_zip
+& 'C:\Program Files\7-Zip\7z.exe' x $cellblender_zip -o"$addon_dir"
 mv cellblender-development cellblender
 $cellblender_dir = "$addon_dir\cellblender"
 
+# Build sbml2json for bng importer
 cd "$cellblender_dir\bng"
 & 'C:\Program Files\7-Zip\7z.exe' x pyinstaller2.zip
 C:\tools\python2\python.exe .\pyinstaller2\pyinstaller.py sbml2json.spec
@@ -41,10 +33,10 @@ cp dist\sbml2json bin\sbml2json.exe
 cd $cellblender_dir
 
 # Get and build MCell (for Windows this time... using MingW)
-$mcell_source = "https://github.com/mcellteam/mcell/archive/v3.3.zip"
-$mcell_destination = "$cellblender_dir\mcell.zip"
-Invoke-WebRequest $mcell_source -OutFile $mcell_destination
-Unzip $mcell_destination $cellblender_dir
+$mcell_download_url = "https://github.com/mcellteam/mcell/archive/v3.3.zip"
+$mcell_zip = "$cellblender_dir\mcell.zip"
+Invoke-WebRequest $mcell_download_url -OutFile $mcell_zip
+& 'C:\Program Files\7-Zip\7z.exe' x $mcell_zip -o"$cellblender_dir"
 mv mcell-3.3 mcell
 $mcell_build_dir =  "$cellblender_dir\mcell\src"
 $cygwin_build_dir = "$project_dir\mcell-3.3\build"
@@ -74,7 +66,7 @@ rm -Force -Recurse "$cellblender_dir\bng\build"
 rm -Force -Recurse "$cellblender_dir\pyinstaller2"
 
 # Zip up modified blender directory
-$final_zip = "$project_dir\blender_final.zip"
+$final_zip = "$project_dir\cellblender_1.1_bundle.zip"
 & 'C:\Program Files\7-Zip\7z.exe' a -mx=9 $final_zip $blender_dir
 
 cd $project_dir
