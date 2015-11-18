c163624c5ac6975974fd195c8b8adb4146e77f3b
Jacob Czech <czech.jacob@gmail.com>
2015-11-18 17:17:04 -0500

    Create zip and do some cleanup.

--- linux/make_cellblender.sh
+++ linux/make_cellblender.sh
@@ -7,7 +7,9 @@
 set -o verbose 
 set -e
 
-blender_dir="blender-2.76b-linux-glibc211-x86_64"
+project_dir=$(pwd)
+blender_dir=blender-2.76b-linux-glibc211-x86_64
+blender_dir_full="$project_dir/blender-2.76b-linux-glibc211-x86_64"
 blender_tar="$blender_dir.tar"
 blender_bz2="$blender_tar.bz2"
 mirror1="http://ftp.halifax.rwth-aachen.de/blender/release/Blender2.76/$blender_bz2";
@@ -29,7 +31,7 @@ rm -fr $blender_tar
 # Need to add userpref.blend, so that CB is enabled by default. Maybe add
 # startup.blend too.
 #cp -fr config $blender_dir/2.76
-cd $blender_dir/2.76/scripts/addons
+cd $blender_dir_full/2.76/scripts/addons
 #git clone https://github.com/mcellteam/cellblender
 wget https://github.com/mcellteam/cellblender/archive/development.zip
 unzip development.zip
@@ -41,8 +43,11 @@ cd cellblender
 sed -i 's/python3\.3/python3/' io_mesh_mcell_mdl/makefile
 sed -i 's/gcc \(-lGL -lglut -lGLU\) \(-o SimControl SimControl.o\)/gcc \2 \1/' makefile
 make
+rm cellblender.zip
+rm cellblender
+rm .gitignore
 
-# Install MCell
+# Get and build MCell
 wget https://github.com/mcellteam/mcell/archive/v3.3.zip
 unzip v3.3.zip
 cd mcell-3.3/src
@@ -58,8 +63,12 @@ rm -fr mcell-3.3 v3.3.zip
 mkdir bin
 mv mcell bin
 
+# Build sbml2json for bng importer
 cd bng
 mkdir bin
 make
 make install
 make clean
+
+cd $project_dir
+zip -r cellblender1.1_bundle_linux.zip $blender_dir_full
