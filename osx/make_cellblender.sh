d09b30ec9baeaa04b44b930ac3c6234d162bea5f
Jacob Czech <czech.jacob@gmail.com>
2016-10-19 10:37:01 -0700

    Remove bootstrap step.
    This step isn't necessary with cmake.

--- osx/make_cellblender.sh
+++ osx/make_cellblender.sh
@@ -96,9 +96,6 @@ mcell_zip_name="v3.4.zip"
 wget https://github.com/mcellteam/mcell/archive/$mcell_zip_name
 unzip $mcell_zip_name
 cd $mcell_dir_name
-cd src
-./bootstrap
-cd ..
 mkdir build
 cd build
 cmake ..
