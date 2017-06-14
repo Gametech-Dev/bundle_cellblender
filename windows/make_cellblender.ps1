45936b60e95956280d0f593c8025c0fa44c6b892
Jacob Czech <czech.jacob@gmail.com>
2017-06-14 11:43:19 -0400

    Update Windows make CB bundle script.
    I'm doing away with the custom matplotlib build, because it was just too
    much trouble to maintain. Also, I'm using anaconda instead of miniconda.
    The compressed build is under 400 MB, which isn't terrible. It's
    unfortunate, because we could really make do with just tk. There's no
    strong incentive to use qt and it's so huge, but we'll live with it now,
    since it will save us a lot of maintenance.
    
    I still have to update some hard coded paths and update the
    instructions, which I'll do in a future commit.

--- windows/make_cellblender.ps1
+++ windows/make_cellblender.ps1
@@ -1,25 +1,26 @@
 ﻿# Change the project dir as needed
 $bl_version = "2.78"
 $bl_minor = ""
-$project_dir = "$home\bundle_cellblender\windows"
-$config_dir = "$home\bundle_cellblender\config"
+#$project_dir = "$home\bundle_cellblender\windows"
+#$config_dir = "$home\bundle_cellblender\config"
+$project_dir = "$home\Documents\GitHub\bundle_cellblender\windows"
+$config_dir = "$home\Documents\GitHub\bundle_cellblender\config"
 #$blender_download_url = "http://ftp.halifax.rwth-aachen.de/blender/release/Blender$bl_version/blender-$bl_version$bl_minor-windows64.zip"
 $blender_download_url = "http://mcell.org/download/files/blender-$bl_version$bl_minor-windows64.zip"
 $blender_zip = "$projectdir\blender.zip"
-$miniconda_dir = "$home\Miniconda3"
-$miniconda_scripts = "$miniconda_dir\Scripts"
+$anaconda_dir = "$home\Anaconda3\envs\cb"
+$anaconda_scripts = "$anaconda_dir\Scripts"
 $mcell_version = "3.4"
 $blender_dir = "$project_dir\blender-$bl_version$bl_minor-windows64"
 $python_dir = "$blender_dir\$bl_version$bl_minor\python"
 $addon_dir = "$blender_dir\$bl_version\scripts\addons"
 $cellblender_dir = "$addon_dir\cellblender"
-$matplotlib_dir = "$project_dir\matplotlib-feedstock"
 
 cd $project_dir
 
 # Ugh. We have to build MCell with cygwin first to create files for the Windows build
-C:\Users\vagrant\.babun\cygwin\bin\dos2unix.exe $project_dir\make_mcell.sh
-C:\Users\vagrant\.babun\cygwin\bin\bash.exe -login $project_dir\make_mcell.sh
+& "$home\.babun\cygwin\bin\dos2unix.exe" $project_dir\make_mcell.sh
+& "$home\.babun\cygwin\bin\bash.exe" -login $project_dir\make_mcell.sh
 
 # Get Blender
 Invoke-WebRequest $blender_download_url -OutFile $blender_zip
@@ -28,17 +29,11 @@ Invoke-WebRequest $blender_download_url -OutFile $blender_zip
 cd $addon_dir
 
 # Get CellBlender
-#$cellblender_download_url = "https://github.com/mcellteam/cellblender/archive/development.zip"
+$cellblender_download_url = "https://github.com/mcellteam/cellblender/archive/development.zip"
 $cellblender_url = "https://github.com/mcellteam/cellblender"
-#$cellblender_zip = "$addon_dir\cellblender.zip"
-#Invoke-WebRequest $cellblender_download_url -Outfile $cellblender_zip
-#& 'C:\Program Files\7-Zip\7z.exe' x $cellblender_zip -o"$addon_dir"
-#mv cellblender-development cellblender
 git clone -q $cellblender_url
 cd cellblender
 git checkout development
-git submodule init
-git submodule update
 
 cd $cellblender_dir
 
@@ -47,8 +42,7 @@ $mcell_download_url = "https://github.com/mcellteam/mcell/archive/v$mcell_versio
 $mcell_zip = "$cellblender_dir\mcell.zip"
 Invoke-WebRequest $mcell_download_url -OutFile $mcell_zip
 & 'C:\Program Files\7-Zip\7z.exe' x $mcell_zip -o"$cellblender_dir"
-mv mcell-$mcell_version mcell
-$mcell_build_dir =  "$cellblender_dir\mcell\src"
+$mcell_build_dir =  "$cellblender_dir\mcell-$mcell_version\src"
 $cygwin_build_dir = "$project_dir\mcell-$mcell_version\build"
 
 cp $cygwin_build_dir\config.h $mcell_build_dir
@@ -61,28 +55,28 @@ cd $mcell_build_dir
 gcc -mconsole -std=c99 -O3 -fno-schedule-insns2 -Wall -Wshadow -o mcell.exe *.c 
 mkdir "$cellblender_dir\bin"
 cp "$mcell_build_dir\mcell.exe" "$cellblender_dir\bin"
+rm -Recurse -Force $mcell_build_dir
+rm $mcell_zip
+
 
 # Replace Blender's Python with Miniconda version and custom matplotlib (w/o Qt)
 # Assume Miniconda was already installed, because chocolatey version isn't working for me
 cd $project_dir
-git clone https://github.com/jczech/matplotlib-feedstock
-cd $miniconda_scripts
-.\conda.exe install -y conda-build
-.\conda.exe install -c SBMLTeam -y python-libsbml
-.\conda.exe install -y m2-patch
-.\conda.exe build $matplotlib_dir\recipe --numpy 1.11
-.\conda.exe install --use-local -y matplotlib
-.\conda.exe build purge
-.\conda.exe clean -y --all
+& "$anaconda_scripts\conda.bat" install -c SBMLTeam -y python-libsbml
+& "$anaconda_scripts\conda.bat" clean -y --all
 rm -Force -Recurse "$python_dir\bin"
 rm -Force -Recurse "$python_dir\lib"
 mkdir $python_dir\bin
-cp -Force -Recurse $miniconda_dir\*.exe $python_dir\bin
-cp -Force -Recurse $miniconda_dir\*.dll $python_dir\bin
-cp -Force -Recurse $miniconda_dir\Lib $python_dir
-cp -Force -Recurse $miniconda_dir\DLLs $python_dir
-cp -Force -Recurse $miniconda_dir\tcl\tcl8.6 $python_dir\Lib
-cp -Force -Recurse $miniconda_dir\tcl\tk8.6 $python_dir\Lib
+cp -Force -Recurse $anaconda_dir\*.exe $python_dir\bin
+cp -Force -Recurse $anaconda_dir\*.dll $python_dir\bin
+cp -Force -Recurse $anaconda_dir\qt.conf $python_dir\bin
+cp -Force -Recurse $anaconda_dir\Lib $python_dir
+cp -Force -Recurse $anaconda_dir\DLLs $python_dir
+#cp -Force -Recurse $anaconda_dir\include $python_dir
+cp -Force -Recurse $anaconda_dir\Library $python_dir
+cp -Force -Recurse $anaconda_dir\tcl\tcl8.6 $python_dir\Lib
+cp -Force -Recurse $anaconda_dir\tcl\tk8.6 $python_dir\Lib
+
 cd $python_dir
 Get-ChildItem -Filter '*.pyc' -Force -Recurse | Remove-Item -Force
 Get-ChildItem -Filter '__pycache__' -Force -Recurse | Remove-Item -Force
@@ -90,19 +84,13 @@ Get-ChildItem -Filter '__pycache__' -Force -Recurse | Remove-Item -Force
 cp -Force -Recurse $config_dir $blender_dir\$bl_version
 
 # Some cleanup
-rm -Force -Recurse "$cellblender_dir\mcell"
+rm -Force -Recurse "$cellblender_dir\mcell-$mcell_version"
 rm -Force -Recurse "$project_dir\mcell-$mcell_version"
 rm -Force "$project_dir\v$mcell_version.zip"
-#rm -Force "$project_dir\blender.zip"
-rm -Force "$cellblender_dir\mcell.zip"
 rm -Force -Recurse "$cellblender_dir\.git"
 rm -Force "$cellblender_dir\.gitignore"
 rm -Force "$cellblender_dir\.gitmodules"
-rm -Force -Recurse "$project_dir\test_suite"
-rm -Force -Recurse "$cellblender_dir\bng\dist"
-rm -Force -Recurse "$cellblender_dir\bng\build"
-rm -Force -Recurse "$cellblender_dir\pyinstaller2"
-rm -Force -Recurse $matplotlib_dir
+rm -Force -Recurse "$cellblender_dir\test_suite"
 
 # Zip up modified blender directory
 $final_zip = "$project_dir\cellblender1.1_bundle_windows.zip"
--- windows/make_mcell.sh
+++ windows/make_mcell.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-cd /cygdrive/c/Users/vagrant/bundle_cellblender/windows
+cd /cygdrive/c/Users/czech/Documents/Github/bundle_cellblender/windows
 
+echo $pwd
 mcell_dir_name="mcell-3.4"
 mcell_zip_name="v3.4.zip"
 #wget https://github.com/mcellteam/mcell/archive/v3.3.zip
